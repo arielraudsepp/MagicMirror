42abe5664a55a8e7c7f15506ffef64e3bcffde08
Ariel Raudsepp <ariel.raudsepp@gmail.com>
2019-02-01 21:24:57 -0700

    Add functions to display new skill every five seconds

--- modules/default/helloworld/helloworld.js
+++ modules/default/helloworld/helloworld.js
@@ -6,19 +6,39 @@
  * By Michael Teeuw http://michaelteeuw.nl
  * MIT Licensed.
  */
+Module.register("helloworld", {
+  skills: [
+    "Wise Mind",
+    "Observe",
+    "Describe",
+    "Participate",
+    "Be non-judgemental",
+    "Be one mindful",
+    "Be effective"
+  ],
+  index: 0,
 
-Module.register("helloworld",{
+  // 
+  start: function() {
+    var self = this;
+	setInterval(function() {
+		self.updateDom(); // no speed defined, so it updates instantly.
+	  }, 5000); //perform every 1000 milliseconds.
+  },
 
-	// Default module config.
-	defaults: {
-		text: "Hello World!"
-	},
-
-	getTemplate: function () {
+  // Displaying skill in the mirror
+  getDom: function() {
+    Log.log("Updating Hello World"); 
+    var wrapper = document.createElement("div");
+	wrapper.innerHTML = this.skills[this.index];
+	this.index = this.index + 1;
+    return wrapper;
+  }
+  /*getTemplate: function () {
 		return "helloworld.njk"
 	},
 
 	getTemplateData: function () {
 		return this.config
-	}
+	}*/
 });
